354b0eaa4b3e7e7825adfdbc2af90b4094870adf
Nick <nicksyz13@gmail.com>
2021-03-18 15:13:30 +0300

    InsertionSort: Done

--- InsertionSortStep/InsertionSortStep.py
+++ InsertionSortStep/InsertionSortStep.py
@@ -1,6 +1,8 @@
 # https://skillsmart.ru/algo/15-121-cm/a94814c2b9.html
 
 def InsertionSortStep(array, step, i):
-    for j in range(i, len(array), step):
-        if array[i] > array[j]:
-            array[i], array[j] = array[j], array[i]
+    j = i
+    for k in range(i, len(array), step):
+        if array[k] < array[j]:
+            j = k
+    array[i], array[j] = array[j], array[i]
--- InsertionSortStep/InsertionSortStep_tests.py
+++ InsertionSortStep/InsertionSortStep_tests.py
@@ -11,4 +11,9 @@ class InsertionSortStepTest(TestCase):
         b = [1,6,5,4,3,2,7,0]
         InsertionSortStep(b, 2, 1)
         
-        self.assertEqual([1,0,5,6,3,4,7,2], b)
+        self.assertEqual([1,0,5,4,3,2,7,6], b)
+
+        c = [7,6,5,4,3,2,1]
+        InsertionSortStep(c, 3, 0)
+        
+        self.assertEqual([1,6,5,4,3,2,7], c)
