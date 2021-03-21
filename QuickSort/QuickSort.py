7f97c3114489b31ae72f05cc0a5064cbb04254fb
Nick <nicksyz13@gmail.com>
2021-03-21 19:04:32 +0300

    QuickSort: ArrayChunk added

--- QuickSort/QuickSort.py
+++ QuickSort/QuickSort.py
@@ -1,25 +1,28 @@
 # https://skillsmart.ru/algo/15-121-cm/e858048f95.html
 
+def ArrayChunk(array, left, right):
+    i = left
+    j = right
+    N = len(array[i:j+1])//2 + i
+    while True:
+        if array[i] < array[N]:
+            i += 1
+        if array[j] > array[N]:
+            j -= 1
+        if i == j-1 and array[i] > array[j]:
+            array[i], array[j] = array[j], array[i]
+            return ArrayChunk(array, left, right)
+        if i == j or (i == j-1 and array[i] < array[j]):
+            return N
+        if array[i] >= array[N] and array[j] <= array[N]:
+            array[i], array[j] = array[j], array[i]
+            if i == N:
+                N = j
+            elif j == N:
+                N = i
+
 def QuickSort(array, left, right):
-    if left < right and left >= 0 and right < len(array):
-        i = left
-        j = right 
-        N = len(array[i:j+1])//2 + i
-        while True:
-            if array[i] < array[N]:
-                i += 1
-            if array[j] > array[N]:
-                j -= 1
-            if i == j-1 and array[i] > array[j]:
-                array[i], array[j] = array[j], array[i]
-                return QuickSort(array, left, right)
-            if i == j or (i == j-1 and array[i] < array[j]):
-                break
-            if array[i] >= array[N] and array[j] <= array[N]:
-                array[i], array[j] = array[j], array[i]
-                if i == N:
-                    N = j
-                elif j == N:
-                    N = i
-        QuickSort(array, left, N-1)
-        QuickSort(array, N+1, right)
+    if left < right:
+        middle = ArrayChunk(array, left, right)
+        QuickSort(array, left, middle-1)
+        QuickSort(array, middle+1, right)
