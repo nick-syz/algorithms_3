9b2f4603ed92d008708a9c335116500b997b5f55
Nick <nicksyz13@gmail.com>
2021-03-21 09:49:02 +0300

    QuickSort: DONE

--- QuickSort/QuickSort.py
+++ QuickSort/QuickSort.py
@@ -1,27 +1,25 @@
 # https://skillsmart.ru/algo/15-121-cm/e858048f95.html
 
 def QuickSort(array, left, right):
-    if left < right:
-        l = left
-        r = right 
-        N = len(array[l:r+1]) // 2 + l
+    if left < right and left >= 0 and right < len(array):
+        i = left
+        j = right 
+        N = len(array[i:j+1])//2 + i
         while True:
-            if array[l] < array[N]:
-                l += 1
-            if array[r] > array[N]:
-                r -= 1
-            if l == r-1 and array[l] > array[r]:
-                array[l], array[r] = array[r], array[l]
+            if array[i] < array[N]:
+                i += 1
+            if array[j] > array[N]:
+                j -= 1
+            if i == j-1 and array[i] > array[j]:
+                array[i], array[j] = array[j], array[i]
                 return QuickSort(array, left, right)
-            if l == r or (l == r-1 and array[l] < array[r]):
+            if i == j or (i == j-1 and array[i] < array[j]):
                 break
-            if array[l] >= array[N] and array[r] <= array[N]:
-                array[l], array[r] = array[r], array[l]
-                if l == N:
-                    N = r
-                elif r == N:
-                    N = l
-        if N-1 >= 0:
-            QuickSort(array, left, N-1)
-        if N+1 < len(array):
-            QuickSort(array, N+1, right)
+            if array[i] >= array[N] and array[j] <= array[N]:
+                array[i], array[j] = array[j], array[i]
+                if i == N:
+                    N = j
+                elif j == N:
+                    N = i
+        QuickSort(array, left, N-1)
+        QuickSort(array, N+1, right)
