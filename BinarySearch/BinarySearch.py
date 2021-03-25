4d87b95f699cf1f0ec48f16f7d00ff98ddf05742
Nick <nicksyz13@gmail.com>
2021-03-25 19:30:13 +0300

    Initial commit

---
+++ BinarySearch/BinarySearch.py
@@ -0,0 +1,28 @@
+# https://skillsmart.ru/algo/15-121-cm/fe994e3320.html
+
+class BinarySearch:
+
+    def __init__(self, array):
+        self.Array = array
+        self.Left = 0
+        self.Right = len(array)-1
+        self.Status = None
+
+    def Step(self, N):
+        middle = self.Array[(self.Left+self.Right)//2]
+        if N == middle:
+            self.Status = 1    
+        else:
+            if N > middle:
+                self.Left = (self.Left+self.Right)//2 + 1
+            else:
+                self.Right = (self.Left+self.Right)//2 - 1
+            if N in (self.Array[self.Right], self.Array[self.Left]):
+                self.Status = 1
+            elif self.Left == self.Right:
+                self.Status = -1
+            else:
+                self.Status = 0
+
+    def GetResult(self):
+        return self.Status
---
+++ BinarySearch/BinarySearch_tests.py
@@ -0,0 +1,30 @@
+from BinarySearch import BinarySearch
+from unittest import TestCase
+
+class BinarySearchTest(TestCase):
+
+    def test_search(self):
+        a = [1,2,3,4,5,6,7]
+        search = BinarySearch(a)
+        search.Step(2)
+        self.assertEqual(0, search.GetResult())
+    
+    def test_search1(self):
+        a = [1,2,3,4,5,6,7]
+        search = BinarySearch(a)
+        search.Step(4)
+        self.assertEqual(1, search.GetResult())
+    
+    def test_search3(self):
+        a = [1,2]
+        search = BinarySearch(a)
+        search.Step(3)
+        self.assertEqual(-1, search.GetResult())
+
+    def test_search4(self):
+        a = [1,2,3,4,5,6,7]
+        search = BinarySearch(a)
+        search.Step(7)
+        while search.GetResult() == 0:
+            search.Step(7)
+        self.assertEqual(1, search.GetResult())
