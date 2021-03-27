cb6a00bbe9741544cce3e0c900aafb177ec4846c
Nick <nicksyz13@gmail.com>
2021-03-27 11:42:13 +0300

    BinSearch2

--- BinarySearch/BinarySearch.py
+++ BinarySearch/BinarySearch.py
@@ -9,23 +9,22 @@ class BinarySearch:
         self.Status = 0
 
     def Step(self, N):
-        middle = (self.Left+self.Right+1)//2
-        self.Status = 0
-        if N == self.Array[middle]:
-            self.Status = 1    
-            return
-        if N > self.Array[middle]:
-            self.Left = middle + 1
-        else:
-            self.Right = middle - 1
-        if self.Left > self.Right:
-            self.Status = -1
-            return
-        if self.Right-self.Left <= 1:
-            if N in (self.Array[self.Left], self.Array[self.Right]):
-                self.Status = 1
+        if self.Status == 0:
+            middle = (self.Left+self.Right+1)//2
+            if self.Left > self.Right:
+                self.Status = -1
                 return
-            self.Status = -1
+            if N == self.Array[middle]:
+                self.Status = 1
+            elif N > self.Array[middle]:
+                self.Left = middle + 1
+            else:
+                self.Right = middle - 1
+            if self.Left > self.Right:
+                self.Status = -1
+            elif self.Left == self.Right and \
+                    N == self.Array[self.Left]:
+                self.Status = 1
 
     def GetResult(self):
         return self.Status
--- BinarySearch/BinarySearch_tests.py
+++ BinarySearch/BinarySearch_tests.py
@@ -8,19 +8,25 @@ class BinarySearchTest(TestCase):
         search = BinarySearch(a)
         search.Step(2)
         self.assertEqual(0, search.GetResult())
-    
+        self.assertEqual(0, search.Left)
+        self.assertEqual(2, search.Right)
+
     def test_search1(self):
         a = [1,2,3,4,5,6,7]
         search = BinarySearch(a)
         search.Step(4)
         self.assertEqual(1, search.GetResult())
-    
+        self.assertEqual(0, search.Left)
+        self.assertEqual(6, search.Right)
+
     def test_search3(self):
         a = [1,2]
         search = BinarySearch(a)
         search.Step(3)
         self.assertEqual(-1, search.GetResult())
-    
+        self.assertEqual(2, search.Left)
+        self.assertEqual(1, search.Right)
+
     def test_search4(self):
         a = [1,2,3,4,5,6,7]
         search = BinarySearch(a)
@@ -28,3 +34,19 @@ class BinarySearchTest(TestCase):
         while search.GetResult() == 0:
             search.Step(7)
         self.assertEqual(1, search.GetResult())
+        self.assertEqual(6, search.Left)
+        self.assertEqual(6, search.Right)
+
+    def test_search5(self):
+        a = [1,2,4,6,7]
+        search = BinarySearch(a)
+        
+        search.Step(3)
+        self.assertEqual(0, search.GetResult())
+        self.assertEqual(0, search.Left)
+        self.assertEqual(1, search.Right)
+        
+        search.Step(3)
+        self.assertEqual(-1, search.GetResult())
+        self.assertEqual(2, search.Left)
+        self.assertEqual(1, search.Right)
